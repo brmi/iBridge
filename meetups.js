f5b3bc5f3df5dfeb4b35edb2a3bb0c111dbc5b67
brmi <desiree@useallfive.com>
2018-11-26 21:30:28 -0800

    Added name to meetups

--- meetups.js
+++ meetups.js
@@ -1,14 +1,27 @@
+var Firestore = InitFirestore();
+
 $(document).ready(function() {
-    var Firestore = InitFirestore();
+    
     const MyID = parseInt(GetCookie("auth"));
     var MaxMeetupID = 0;
+    let firstname = 'NO FIRSTNAME';
+    let lastname = 'NO LASTNAME';
 
     Firestore.collection("Meetups").get().then(function(Query){
         Query.forEach(function(Doc) {
             const Meetup = Doc.data();
-            const MeetupItem = CreateMeetupItem(Meetup.name, Meetup.description, Meetup.location, Meetup.time, Meetup.meetupid, Meetup.createdby == MyID);
+            const MeetupItem = CreateMeetupItem(Meetup.name, Meetup.description, Meetup.location, Meetup.time, Meetup.meetupid, Meetup.firstname, Meetup.lastname, Meetup.createdby == MyID);
             $(".meetups").prepend(MeetupItem);
             MaxMeetupID = Math.max(MaxMeetupID, Meetup.meetupid);
+        });
+    });
+
+    // Get Profile First & Last Name
+    Firestore.collection("Profiles").where("id", "==", MyID).get().then(function(Query){
+        Query.forEach(function(Doc){
+            const Profile = Doc.data();
+            firstname = Profile.first;
+            lastname = Profile.last;
         });
     });
 
@@ -25,7 +38,10 @@ $(document).ready(function() {
         const Description = $("#in-description").val();
         const Location = $("#in-location").val();
         const Time = $("#in-time").val();
-        const MeetupItem = CreateMeetupItem(Title, Description, Location, Time, ++MaxMeetupID, true);
+
+        
+        // const MeetupUsername = GetUsername();
+        const MeetupItem = CreateMeetupItem(Title, Description, Location, Time, ++MaxMeetupID, firstname, lastname, true);
 
         if (Title == "" || Description == "" || Location == "" || Time == "") {
             // @todo Error box
@@ -37,7 +53,9 @@ $(document).ready(function() {
                 location: Location,
                 time: Time,
                 createdby: MyID,
-                meetupid: MaxMeetupID
+                meetupid: MaxMeetupID,
+                firstname: firstname,
+                lastname: lastname
             });
             $(".meetups").prepend(MeetupItem);
         }
@@ -45,7 +63,7 @@ $(document).ready(function() {
         $('.create-meetup').css({'display': 'none'});
     });
 
-    $(".meetups").on("click", ".CancelMeetup", function(){
+    $(".meetups").on("click", ".cancel-meetup", function(){
         const MeetupID = parseInt($(this).val());
         Firestore.collection("Meetups").where("meetupid", "==", MeetupID).get().then(function(Query){
             Query.forEach(function(Doc){
@@ -57,12 +75,14 @@ $(document).ready(function() {
     });
 });
 
-function CreateMeetupItem(Name, Description, Location, Time, MeetupID, bCreatedByMe) {
-    const Button = bCreatedByMe ? `<button class='CancelMeetup' value="` + MeetupID + `">Cancel Meetup</button>` : `<button class='attend' value="` + MeetupID + `">attend</button>`;
+function CreateMeetupItem(Name, Description, Location, Time, MeetupID, MeetupFirstname, MeetupLastname, bCreatedByMe) {
+    console.log(MeetupFirstname, MeetupLastname);
+    const Button = bCreatedByMe ? `<button class='cancel-meetup' value="` + MeetupID + `">Cancel</button>` : `<button class='attend' value="` + MeetupID + `">attend</button>`;
     const MeetupItem = `
         <div class='meetup-item'>
             <div class='profile-pic'>
                 <img src='./images/kermit.png'/>
+                <p class='name'>`+ MeetupFirstname +` ` + MeetupLastname + `</p>
             </div>
             <div class='event'>
                 <h2 class='meetup-title'>` + Name + `</h2>
